9651bf99973c52fd400de2817632b8d9acb89245
oscar <ing_oscar_naranjo@hotmail.com>
2021-05-20 17:16:16 -0500

    Uktima actualización 20-05-2021

--- index.php
+++ index.php
@@ -11,17 +11,17 @@
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" class="form" action="login.php" method="post">
-                            <h3 class="text-center text-info">Ingreso al Sistemas ISTVN</h3>
+                            <h3 class="text-center text-info">Ingreso al Sistemas Instituto Superior Tecnológico Vida Nueva</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
+                                <label for="username" class="text-info">Usuario:</label><br>
                                 <input type="text" name="username" id="username" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
+                                <label for="password" class="text-info">Contraseña:</label><br>
                                 <input type="text" name="password" id="password" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <label for="remember-me" class="text-info"><span>Repetir contraseña</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="Ingresar">
                             </div>
                             <div id="register-link" class="text-right">
